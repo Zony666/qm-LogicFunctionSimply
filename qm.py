b67777fce87837912929620c34d45562cf52835b
Zone <80498210+Zony666@users.noreply.github.com>
2022-01-24 17:26:50 +0800

    Add files via upload
    添加 有无关项的化简

--- qm.py
+++ qm.py
@@ -11,18 +11,27 @@ import numpy as np
 import sys
 
 class qm():
-    def __init__(self,num_virable,minterms:list,varname:list):
+    # num_virable:变量名个数，varname:变量名列表，minterms:最小项代号，irrterms:无关项代号   
+    def __init__(self,num_virable,varname:list,minterms:list,irrterms:list=None):
+        # T记录合并过程产生的表
         self.T=[]
         self.varname=varname
+        self.irr=irrterms
+        # G0初始表
         self.G0={}
         self.mins=minterms
-        minterms=self.trans2bin(num_virable,minterms)
-        for minterm in minterms:
+        # 初始化表
+        if irrterms:
+            allterms=self.trans2bin(num_virable,minterms+irrterms)
+        else:
+            allterms=self.trans2bin(num_virable,minterms)
+        for minterm in allterms:
             num1=self.get1num(minterm[1])
             if num1 not in self.G0.keys():
                 self.G0[num1]=[]
             # 0 待定，1已合并，-1 未合并
             self.G0[num1].append([[minterm[0]],minterm[1],0])  
+    #合并产生新表
     def __merge__(self,G):
         newg = {}
         t=[]
@@ -30,13 +39,11 @@ class qm():
         gids=list(G.keys())
         gids.sort()
         for gid in gids[0:-1]:
-            #print(gid)
             if gid+1 not in gids:
                 for minc in G[gid]:
                     minc[2]=-1
                 continue
             for minc in G[gid]:
-                # print(minc)
                 for minn in G[gid+1]:
                     minvec=self.cmp1d(minc[1],minn[1])
                     if minvec:
@@ -53,6 +60,7 @@ class qm():
                             minc[2]=-1
                         continue
         return newg
+    # 判断合并是否结束
     def judge(self,tg:dict):
         num=0
         tnum=0
@@ -65,6 +73,7 @@ class qm():
             return False
         else:
             return True
+    # 删除重复项
     def delrepeat(self,g:list):
         gc=g.copy()
         ng=[]
@@ -77,6 +86,7 @@ class qm():
             ng[i][0]=list(ng[i][0])
                
         return ng
+    # 合并过程
     def combine(self):
         g1=self.__merge__(self.G0)
         self.T.append(self.G0)
@@ -91,6 +101,7 @@ class qm():
                     t=n
                 else:
                     break
+    # 转二进制
     def trans2bin(self,num_virable,vals:list):
         res=[]
         for val in vals:
@@ -98,12 +109,14 @@ class qm():
             r=v.zfill(num_virable)
             res.append([val,r])
         return res
+    # 获取‘1’的数量
     def get1num(self,bindata):
         num=0
         for i in bindata:
             if i=='1':
                 num+=1
         return num
+    # 比较两二进制数据不同
     def cmp1d(self,bindata1,bindata2):
         num=0
         nbindata=''
@@ -115,13 +128,18 @@ class qm():
                 nbindata+=data
         if num==1:
             return nbindata
+    # 素项表构造并划线
     def getPrimeTable(self):
         primeterm=[]
+        # print(self.T)
         for t in self.T:
-            for gid in t.keys():
-                for minc in t[gid]:
-                    if minc[2]==-1 or minc[2]==0:
-                        primeterm.append(minc[0:2])
+            if t:
+                for gid in t.keys():
+                    for minc in t[gid]:
+                        if minc[2]==-1 or minc[2]==0:
+                            if self.irr:
+                                if not set(minc[0])<set(self.irr):
+                                    primeterm.append(minc[0:2])
         # print(primeterm)
         allprimin=[]
         for t in primeterm:
@@ -132,7 +150,7 @@ class qm():
         for i in s:
             c=allprimin.count(i)
             prinum[i]=c
-        #print(prinum)
+        # print(prinum)
         # 单项位置
         pric=[]
         prir=[]
@@ -140,14 +158,17 @@ class qm():
             for d in range(len(prinum)):
                 if list(prinum.values())[d]==1:
                     minv=list(prinum.keys())[d]
-                    pric.append(self.mins.index(minv))
+                    if minv in self.mins:
+                        pric.append(self.mins.index(minv))
                     
                     for i in range(len(primeterm)):
                         if minv in primeterm[i][0]:
                             prir.append(i)
         # print(pric,prir)
+        else:
+            pass
         
-        
+        # 素项表
         primet=np.zeros([len(primeterm),len(self.mins)])
         for i in range(len(primeterm)):
             for j in range(len(self.mins)):
@@ -178,6 +199,7 @@ class qm():
         for i in ridx:
             r.append(primeterm[i][1])
         return r
+    # 替换变量字符
     def translate(self,r:list[str]):
         res=''
         fname=[s+'\'' for s in self.varname]
@@ -195,6 +217,7 @@ class qm():
                     tri+=''
             res+=('+'+tri)
         return res[1:]      
+    # 输出化简结果
     def simply(self):
         self.combine()
         r=self.getPrimeTable()
@@ -209,21 +232,25 @@ if __name__ =='__main__':
             print('qm.py 变量个数 变量名称 最小项下标-----(,分隔)')
             exit()
         else:
-            varnum=int(args[0])
-    if len(args)<3:
+            varname=args[0].split(',')
+            varnum=len(varname)
+    if len(args)<2:
         print('请输入完整参数（help 查看使用帮助）')
         exit()
     if args[1]:
-        varname=args[1].split(',')
-
-    if args[2]:
-        mins=args[2]
+        mins=args[1]
         mins=mins.split(',')
         minw=[int(i) for i in mins]
         minterm=minw
     
-    
-
-    g=qm(varnum,minterm,varname)
+    if len(args)==3:
+        irrt=args[2]
+        irrt=irrt.split(',')
+        irret=[int(i) for i in irrt]
+        irreleventTerm=irret
+        g=qm(varnum,varname,minterm,irreleventTerm)       
+    else:
+        g=qm(varnum,varname,minterm)
+        
     print(g.translate(g.simply()))
 
